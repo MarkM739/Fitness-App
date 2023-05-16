ca65eba8a150c7473d81534a5d2d43ffbd5d3863
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2023-05-16 19:46:44 -0230

    Router edits

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Workouts from './components/Workouts'
 import Profile from "./components/Profile";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 
 const exercises = [
@@ -23,12 +24,35 @@ function App() {
   
 
   return (
+<div>
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/workouts">Workouts</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/workouts" component={Workouts} />
+      <Route path="/profile" component={Profile} />
+  </div>
+</Router>
     <div>
       <Workouts exercises={exercises} onSelect={handleExerciseSelect} selectedExercise={selectedExercise} />
       <Profile></Profile>
       <Navbar></Navbar>
     </div>
-  )
+    </div>
+  );
     
    
   
