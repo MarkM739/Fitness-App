90ccc86179b5f91c74f3dad68cde591a306aa20c
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2023-06-06 20:16:45 -0230

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,16 @@ const Navbar: React.FC<NavbarProps> = () => {
   </nav>
 );
 };
+<div class="bg-gray-200 p-8">
+  <h1 class="text-3xl font-bold mb-4">Welcome to My Homepage</h1>
+  <p class="text-lg mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
+  <div class="flex justify-center">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Get Started
+    </button>
+  </div>
+</div>
 export default Navbar;
 
 
